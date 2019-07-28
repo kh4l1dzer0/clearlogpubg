581fca2d4c43a944301ded17e0420ba5129c28b8
kh4l1dzer0 <46495208+kh4l1dzer0@users.noreply.github.com>
2019-07-28 17:40:05 +0700

    Create clear.sh
    install
    git clone https://github.com/kh4l1dzer0/clearlogpubg/clear.sh
    cd clearlogpubg
    sh clear.sh

---
+++ clear.sh
@@ -0,0 +1,53 @@
+toilet -f standard "Khalid Abraham" -F gay
+
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/ca-bundle.pem
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/cacheFile.txt
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/login-identifier.txt
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/vmpcloudconfig.json
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/ProgramBinaryCache
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/1375135419_47_0.13.0.11098_20190617165748_1981158817_cures.ifs.res
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Pandora
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir
+rm -rf /storage/emulated/0/tencent
+
+uptime 
+
+
+echo "║▓░░░░░░░░░░░░░░║"
+					 
+echo "║▓▓░░░░░░░░░░░░░║"
+					 
+echo "║▓▓▓░░░░░░░░░░░░║"
+			
+echo "║▓▓▓▓░░░░░░░░░░░║"
+			
+echo "║▓▓▓▓▓░░░░░░░░░░║"
+	
+echo "║▓▓▓▓▓▓░░░░░░░░░║"
+	
+echo "║▓▓▓▓▓▓▓░░░░░░░░║"
+	
+echo "║▓▓▓▓▓▓▓▓░░░░░░░║"
+
+echo "║▓▓▓▓▓▓▓▓▓░░░░░░║"
+					
+echo "║▓▓▓▓▓▓▓▓▓▓░░░░░║"
+					
+echo "║▓▓▓▓▓▓▓▓▓▓▓░░░░║"
+					 
+echo "║▓▓▓▓▓▓▓▓▓▓▓▓░░░║"
+				
+echo "║▓▓▓▓▓▓▓▓▓▓▓▓▓░░║"
+			
+echo "║▓▓▓▓▓▓▓▓▓▓▓▓▓▓░║"
+					
+echo "║▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓║"
+
+toilet -f standard "BERES BOSS!!" -F gay
